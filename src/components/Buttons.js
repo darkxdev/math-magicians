af72ec73567e3c1210e4a8fd714a26c40e8b3aec
DarkxDev <bryanfernandohurtado@gmail.com>
2023-02-15 12:53:46 -0500

    Added events for the calculator logic

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,35 +1,76 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Buttons = () => (
+const Buttons = ({ handleClick }) => (
   <div className="button-grid">
     <div className="upper-button-grid">
-      <button className="btn-grey button" type="button">AC</button>
-      <button className="btn-grey button" type="button">+/-</button>
-      <button className="btn-grey button" type="button">%</button>
-      <button className="btn-orange button" type="button">÷</button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('AC')}>
+        AC
+      </button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('+/-')}>
+        +/-
+      </button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('%')}>
+        %
+      </button>
+      <button className="btn-orange button" type="button" onClick={() => handleClick('÷')}>
+        ÷
+      </button>
 
-      <button className="btn-grey button" type="button">7</button>
-      <button className="btn-grey button" type="button">8</button>
-      <button className="btn-grey button" type="button">9</button>
-      <button className="btn-orange button" type="button">x</button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('7')}>
+        7
+      </button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('8')}>
+        8
+      </button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('9')}>
+        9
+      </button>
+      <button className="btn-orange button" type="button" onClick={() => handleClick('x')}>
+        x
+      </button>
 
-      <button className="btn-grey button" type="button">4</button>
-      <button className="btn-grey button" type="button">5</button>
-      <button className="btn-grey button" type="button">6</button>
-      <button className="btn-orange button" type="button">-</button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('4')}>
+        4
+      </button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('5')}>
+        5
+      </button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('6')}>
+        6
+      </button>
+      <button className="btn-orange button" type="button" onClick={() => handleClick('-')}>
+        -
+      </button>
 
-      <button className="btn-grey button" type="button">1</button>
-      <button className="btn-grey button" type="button">2</button>
-      <button className="btn-grey button" type="button">3</button>
-      <button className="btn-orange button" type="button">+</button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('1')}>
+        1
+      </button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('2')}>
+        2
+      </button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('3')}>
+        3
+      </button>
+      <button className="btn-orange button" type="button" onClick={() => handleClick('+')}>
+        +
+      </button>
     </div>
 
     <div className="lower-button-grid">
-      <button className="btn-grey button" type="button">0</button>
-      <button className="btn-grey button" type="button">.</button>
-      <button className="btn-orange button" type="button">=</button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('0')}>
+        0
+      </button>
+      <button className="btn-grey button" type="button" onClick={() => handleClick('.')}>
+        .
+      </button>
+      <button className="btn-orange button" type="button" onClick={() => handleClick('=')}>
+        =
+      </button>
     </div>
   </div>
 );
+
+Buttons.propTypes = { handleClick: PropTypes.func.isRequired };
 
 export default Buttons;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Buttons from './Buttons';
 import Display from './Display';
+import calculate from '../logic/calculate';
 
-const CalculatorLayout = () => (
-  <div className="calculator">
-    <Display />
-    <Buttons />
-  </div>
-);
+const CalculatorLayout = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+
+  const handleClick = (buttonName) => {
+    const result = calculate({ total, next, operation }, buttonName);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
+  };
+
+  return (
+    <div className="calculator">
+      <Display value={next || total || '0'} />
+      <Buttons handleClick={handleClick} />
+    </div>
+  );
+};
 
 export default CalculatorLayout;
